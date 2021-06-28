e1b26c43fd608634e4593aa383323c0ce85e9a49
Simon <simonrosenberg1@gmail.com>
2021-06-28 18:02:13 +0100

    Feat: Add Apollo client to server.js

--- App.tsx
+++ App.tsx
@@ -1,37 +1,41 @@
-import React, {} from 'react';
+import React, { } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ApolloProvider } from '@apollo/react-hooks'
 import BrowseScreen from './screens/BrowseScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ConnectionsStack from './screens/ConnectionsStack';
+import client from './client'
 
 
 export default function App() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeBackgroundColor: '#dddddd',
-          tabStyle: styles.tab,
-          labelStyle: styles.tabText,
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeBackgroundColor: '#dddddd',
+            tabStyle: styles.tab,
+            labelStyle: styles.tabText,
           }}>
-        <Tab.Screen 
-          name="Browse" 
-          component={BrowseScreen}
-        />
-        <Tab.Screen 
-          name="Connections" 
-          component={ConnectionsStack}
-        />
-        <Tab.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen
+            name="Browse"
+            component={BrowseScreen}
+          />
+          <Tab.Screen
+            name="Connections"
+            component={ConnectionsStack}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
