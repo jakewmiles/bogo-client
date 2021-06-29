aa79c57acef3d5705153da2ccd00a2f6afa10f1c
Simon <simonrosenberg1@gmail.com>
2021-06-29 17:13:39 +0100

    Feat: Add interests and languages to profile page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -30,7 +30,27 @@ const mock = {
   city: 'Dartford',
   country: 'England',
   rating: 4.5,
-  content: 'If falling asleep on trains after a great night out at a pub is your thing. I\'d be happy to introduce some local brews!'
+  content: 'If falling asleep on trains after a great night out at a pub is your thing. I\'d be happy to introduce some local brews!',
+  hangout1: 'https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68',
+  hangout2: 'https://i.picsum.photos/id/1016/3844/2563.jpg?hmac=WEryKFRvTdeae2aUrY-DHscSmZuyYI9jd_-p94stBvc',
+  interests: ['Music', 'Hiking', 'Films', 'Football', 'Cooking'],
+  languages: [{ name: 'English', level: 'Fluent' }, { name: 'Chinese', level: 'Conversational' }]
+}
+
+let interestsString = '';
+mock.interests.forEach(interest => {
+  interestsString = interestsString + interest + ', ';
+  return;
+})
+if (interestsString) {
+  interestsString = interestsString.slice(0, -2);
+}
+let languagesString = '';
+mock.languages.forEach(language => {
+  languagesString = languagesString + language.level + ' ' + language.name + ', ';
+})
+if (languagesString) {
+  languagesString = languagesString.slice(0, -2);
 }
 
 const Profile = (props: Props) => {
@@ -110,6 +130,14 @@ const Profile = (props: Props) => {
             <Text style={styles.hangoutButtonText}>Add +</Text>
           </TouchableOpacity>
         </View>
+        <View style={styles.hangoutImages}>
+          <Image source={{ uri: mock.hangout1 }} style={styles.hangoutImage} />
+          <Image source={{ uri: mock.hangout2 }} style={styles.hangoutImage} />
+        </View>
+        <Text style={styles.categoriesHeading}>Interests</Text>
+        <Text style={styles.categories}>{interestsString}</Text>
+        <Text style={styles.categoriesHeading}>Speaks</Text>
+        <Text style={styles.categories}>{languagesString}</Text>
         {/* <ScrollView contentContainerStyle={styles.scroll}>
         <Text style={styles.mainHeader}>{profileInfo.first_name}</Text>
         <Image
@@ -137,18 +165,19 @@ const Profile = (props: Props) => {
 const styles = StyleSheet.create({
   view: {
     backgroundColor: '#ffffff',
-    height: 550,
+    height: 580,
     width: '95%',
     borderRadius: 8,
     paddingHorizontal: 15,
-    paddingVertical: 20,
+    paddingVertical: 15,
     shadowOffset: { width: 0, height: 4 },
     shadowColor: 'rgba(0, 0, 0, .25)',
     shadowOpacity: 1,
+    elevation: 3,
   },
   profileHeader: {
     flexDirection: 'row',
-    height: 120,
+    height: 100,
     justifyContent: 'space-between',
     alignItems: 'flex-start',
   },
@@ -172,7 +201,7 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     paddingLeft: 10,
     width: '60%',
-    height: 100,
+    height: 85,
   },
   content: {
     height: 130,
@@ -200,6 +229,29 @@ const styles = StyleSheet.create({
     height: '100%',
     paddingHorizontal: 20,
     justifyContent: 'center',
+  },
+  hangoutImages: {
+    marginVertical: 20,
+    height: 80,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  hangoutImage: {
+    height: 80,
+    width: '47%',
+    borderRadius: 6,
+  },
+  categoriesHeading: {
+    fontFamily: 'PTSans_400Regular',
+    fontSize: 20,
+    color: '#120E21',
+  },
+  categories: {
+    fontFamily: 'PTSans_400Regular',
+    fontSize: 18,
+    color: '#99879D',
+    marginBottom: 10,
   },
   // scroll: {
   //   width: '100%',
