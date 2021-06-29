a9e8b5d42f4bfac2978c155393483c368a438b76
Jake <jake.w.miles@gmail.com>
2021-06-29 22:51:43 +0100

    feat: location fetching from map

--- screens/SignUp/BirthDateScreen.tsx
+++ screens/SignUp/BirthDateScreen.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import IconButton from '../../components/IconButton';
-// import MapView from 'react-native-maps';
+import MapView, { LatLng, Marker } from 'react-native-maps';
 
 export interface BirthDateScreenProps {
   navigation: any;
@@ -12,6 +12,8 @@ export interface BirthDateScreenProps {
 const BirthDateScreen: React.FC<BirthDateScreenProps> = ({ navigation }) => {
   const [date, setDate] = useState(new Date());
   const [show, setShow] = useState(false);
+  const [marker, setMarker] = useState<LatLng>({latitude: 0, longitude: 0});
+  const [location, setLocation] = useState('')
   
   const onChange = (event: Event, selectedDate: Date) => {
     const currentDate = selectedDate || date;
@@ -34,6 +36,30 @@ const BirthDateScreen: React.FC<BirthDateScreenProps> = ({ navigation }) => {
         onChange={onChange}
       />)}
       <Text style={styles.text}>Where are you from?</Text>
+      <MapView 
+        style={styles.map}
+        initialRegion={{
+          latitude: 51.49492,
+          longitude: -0.12766,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}
+        onLongPress={async (e) => {
+          setMarker(e.nativeEvent.coordinate)
+          let latitude = String(e.nativeEvent.coordinate.latitude);
+          let longitude = String(e.nativeEvent.coordinate.longitude);
+          if (Number(latitude) > 0) latitude = '+' + latitude;
+          if (Number(longitude) > 0) longitude = '+' + longitude;
+          const response = await fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=5&offset=0&location=${encodeURIComponent(latitude)}${encodeURIComponent(longitude)}&radius=20&sort=-population`)
+          const location = await response.json();
+          if (!location.data.length) setLocation('No major cities near your location');
+          else setLocation(`${location.data[0].city}, ${location.data[0].country}`);
+        }}
+      >
+        <Marker coordinate={marker}/>
+      </MapView>
+      <Text>{location}</Text>
+      
       <TouchableOpacity 
         onPress={() => navigation.navigate('LocationScreen')}
       >
@@ -51,7 +77,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "bold",
-    marginVertical: 50,
+    marginVertical: 30,
   },
   datePicker: {
     fontSize: 20,
@@ -60,8 +86,13 @@ const styles = StyleSheet.create({
     color: '#655669',
     paddingHorizontal: '15%',
     paddingVertical: 10,
-
   },
+  map: {
+    width: Dimensions.get('window').width - 100,
+    height: Dimensions.get('window').height - 400,
+    marginBottom: 25,
+    borderWidth: 1,
+  }
 })
 
 export default BirthDateScreen;
