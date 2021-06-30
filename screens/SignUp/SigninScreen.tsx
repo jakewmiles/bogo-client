b77b85a66b9176b16046669abb8d6a2491cf339f
gerseymour <george.anthony.seymour@gmail.com>
2021-06-30 15:14:46 +0100

    feat: css of signin page

--- screens/SignUp/SigninScreen.tsx
+++ screens/SignUp/SigninScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,TextInput, TouchableOpacity } from 'react-native';
+import { Formik } from 'formik';
+import FloatingCard from '../../components/FloatingCard';
 import TextButton from '../../components/TextButton';
 
 export interface SigninScreenProps {
@@ -10,12 +12,41 @@ export interface SigninScreenProps {
 const SigninScreen: React.FC<SigninScreenProps> = ({ navigation }) => {
   return ( 
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <TouchableOpacity 
-        style={styles.button}
-        onPress={() => navigation.navigate('LandingScreen')}
+      <Formik
+        initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+        onSubmit={values => console.log(values)}
       >
-        <TextButton title={'Sign in'}/>
-      </TouchableOpacity>
+        {({ handleChange, handleBlur, handleSubmit, values}) => (
+          <View style={{width: '90%', justifyContent: 'center', alignItems: 'center'}}>
+            <FloatingCard cardWidth={'90%'}>
+              <Text style={{ fontSize: 32, marginVertical: '10%' }}>Sign-in</Text>
+              <TextInput 
+                placeholder="Email"
+                style={styles.inputField}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              <TextInput 
+                placeholder="Password"
+                secureTextEntry={true}
+                style={styles.inputField}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+            </FloatingCard>
+            <TouchableOpacity 
+              style={styles.button}
+              onPress={() => {
+                if(!values.email || !values.password) alert('FAIL')
+                else navigation.navigate('Home');
+              }}>
+              <TextButton title={'Sign in'}/>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
     </View>
    );
 }
@@ -23,7 +54,21 @@ const SigninScreen: React.FC<SigninScreenProps> = ({ navigation }) => {
 const styles = StyleSheet.create({
   button: {
     width: '70%',
-    height: '7%',
+    height: 55,
+    marginTop: '25%',
+  },
+  wholeForm: {
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#99879D',
+    borderRadius: 15,
+    width: '90%',
+  },
+  inputField: {
+    backgroundColor: '#99879D21',
+    width: '85%',
+    padding: '5%',
+    marginBottom: '8%'
   }
 })
  
